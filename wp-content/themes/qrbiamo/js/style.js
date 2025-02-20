cab72326b26883512455bbae086e7fc3f1227962
AnhDungIT-09 <ledinhdung.net@gmail.com>
2025-02-20 16:52:41 +0700

    first commit

---
+++ wp-content/themes/qrbiamo/js/style.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	$('.owl-carousel').owlCarousel({
+	    loop:true,
+	    autoplay:true,
+	    margin:10,
+	    nav:true,
+	    autoplayHoverPause:true,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        440:{
+	            items:2
+	        },
+	        1000:{
+	            items:3
+	        }
+	    }
+	})
+})
